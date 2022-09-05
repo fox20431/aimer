9ff694b98fd6e14a36bffae63fbe6a11df9f94b6
ming <fox20431@gmail.com>
2022-09-05 22:59:09 +0800

    Used shadow root to scope the style, and finished the timer roughly.

--- projects/timer/src/lib/timer.component.ts
+++ projects/timer/src/lib/timer.component.ts
@@ -1,20 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Directive, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+
+
 
 @Component({
-  selector: 'timer',
-  template: `
-    <p>
-      timer works!
-    </p>
-  `,
-  styles: [
-  ]
+	selector: 'lib-timer',
+	templateUrl: './timer.component.html',
+	styleUrls: ['./timer.component.scss'],
+	encapsulation: ViewEncapsulation.ShadowDom
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+	@ViewChild('top')
+	topElement!: ElementRef<HTMLParagraphElement>;
+	@ViewChild('flodBox')
+	flodBoxElement!: ElementRef<HTMLParagraphElement>;
+	@ViewChild('front')
+	frontElement!: ElementRef<HTMLParagraphElement>;
+	@ViewChild('back')
+	backElement!: ElementRef<HTMLParagraphElement>;
+	@ViewChild('bottom')
+	bottomElement!: ElementRef<HTMLParagraphElement>;
 
-  ngOnInit(): void {
-  }
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+	ngAfterViewInit(): void {
+		const transform180 = (seconds: any) =>{
+			let timer: any;
+			this.flodBoxElement.nativeElement.style.transition= "transform 0s"; //使其瞬间返回到 0deg
+			this.flodBoxElement.nativeElement.style.transform = "perspective(200px) rotateX(0deg)";
+			// frontNode 与 bottomNode 的显示
+			this.frontElement.nativeElement.innerHTML = seconds;
+			this.bottomElement.nativeElement.innerHTML = seconds;
+			timer = setTimeout(()=>{
+				this.flodBoxElement.nativeElement.style.transition= "transform 0.9s"; //缓个100ms执行，0.9秒转完
+				this.topElement.nativeElement.innerHTML = seconds + 1 == 60 ? 0 :seconds + 1 ;
+				this.backElement.nativeElement.innerHTML = seconds + 1 == 60 ? 0 :seconds + 1 ;
+				this.flodBoxElement.nativeElement.style.transform = "perspective(200px) rotateX(-180deg)";
+				clearTimeout(timer);
+			},100)
+		}
+		setInterval(()=>{
+      let seconds = new Date().getSeconds();
+      transform180(seconds);
+    },1000);
+	}
 
 }
--- projects/wingv/src/app/index/index.component.ts
+++ projects/wingv/src/app/index/index.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, AfterViewInit {
 
 	title = "纪念日"
 
-  constructor() { }
+  constructor() { 
+	}
+	ngAfterViewInit(): void {
+	}
 
   ngOnInit(): void {
   }
