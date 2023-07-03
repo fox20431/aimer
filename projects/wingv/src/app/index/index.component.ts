035529093d12f3f4b380952830a0228282f4d91a
ming <fox20431@gmail.com>
2023-07-04 03:03:19 +0800

    fixed the day and readme

--- projects/wingv/src/app/index/index.component.ts
+++ projects/wingv/src/app/index/index.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 function calcDaysFromMeeting() {
-	let meetingTimestamp = new Date("9/9/22").getTime();
+	let meetingTimestamp = new Date("6/12/23").getTime();
 	let nowTimestamp = Date.now()
 	let interval = nowTimestamp - meetingTimestamp
 	return Math.floor(interval / 1000 / 60 / 60 / 24);
